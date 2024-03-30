a9d372348cb38624bd714cf212da3f951e4774cc
Babkina-Ekaterina <bkatya5577@yandex.ru>
2024-03-31 02:00:53 +0300

    Add controller

---
+++ src/main/java/ru/neoflex/calculator/controller/HolidayPayCalculatorController.java
@@ -0,0 +1,32 @@
+package ru.neoflex.calculator.controller;
+
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+import ru.neoflex.calculator.data.dto.HolidayPayResponseDto;
+import ru.neoflex.calculator.service.HolidayPayCalculatorService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import java.time.LocalDate;
+
+@RestController
+@RequestMapping("/api/holiday_pay")
+@AllArgsConstructor
+@CrossOrigin(origins = {"http://localhost:3000"}, allowCredentials = "true")
+public class HolidayPayCalculatorController {
+
+    private final HolidayPayCalculatorService holidayPayCalculatorService;
+
+    @GetMapping("/calculate")
+    public ResponseEntity<HolidayPayResponseDto> calculateHolidayPay(@RequestParam double averageSalary,
+                                                                   @RequestParam LocalDate holidayStartDate,
+                                                                   @RequestParam LocalDate holidayEndDate) {
+        HolidayPayResponseDto holidayPayResponseDto = holidayPayCalculatorService.calculateHolidayPay(
+                averageSalary, holidayStartDate, holidayEndDate);
+
+        if (holidayPayResponseDto != null) {
+            return new ResponseEntity<>(holidayPayResponseDto, HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+    }
+}

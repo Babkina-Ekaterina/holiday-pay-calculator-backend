6c274219d3f03514c4536f51aecdb87efe675088
Babkina-Ekaterina <bkatya5577@yandex.ru>
2024-03-31 01:37:59 +0300

    Initialize project

---
+++ src/main/java/ru/neoflex/calculator/HolidayPayCalculatorApplication.java
@@ -0,0 +1,12 @@
+package ru.neoflex.calculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HolidayPayCalculatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HolidayPayCalculatorApplication.class, args);
+    }
+}

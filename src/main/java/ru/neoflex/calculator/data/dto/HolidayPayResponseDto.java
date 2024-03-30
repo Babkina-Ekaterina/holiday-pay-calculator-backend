515574e84f1b081f3e05d09a00b4d3989a48960c
Babkina-Ekaterina <bkatya5577@yandex.ru>
2024-03-31 02:00:25 +0300

    Add response DTO

---
+++ src/main/java/ru/neoflex/calculator/data/dto/HolidayPayResponseDto.java
@@ -0,0 +1,11 @@
+package ru.neoflex.calculator.data.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class HolidayPayResponseDto {
+
+    private double vacationPay;
+}
